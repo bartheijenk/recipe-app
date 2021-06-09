9813e45f3e52aa2436d1a09c7cb323fac730eed1
bartheijenk <bdheijenk@gmail.com>
2021-06-09 17:03:10 +0200

    changed some stuff in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { ReceptResolver } from './core/resolvers/recept.resolver';
+import { ReceptDetailComponent } from './modules/recepten/recept-detail/recept-detail.component';
 import { ReceptInvoerComponent } from './modules/recepten/recept-invoer/recept-invoer.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
   { path: 'recepten', loadChildren: () => import('./modules/recepten/recepten.module').then(m => m.ReceptenModule) },
-  {
-    path: 'invoer', component: ReceptInvoerComponent, /*canActivate: [AuthGuard],*/
-    children: [
-      { path: ':id', component: ReceptInvoerComponent, resolve: { recept: ReceptResolver } }
-    ]
-  },
-];
+  {path: 'invoer', component: ReceptInvoerComponent, /*canActivate: [AuthGuard],*/},
+    { path: 'invoer/:id', component: ReceptInvoerComponent,
+        resolve: {
+          recept: ReceptResolver
+        }}
+      ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
