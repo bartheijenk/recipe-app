f138781188ebea95b966be4dbc510e67365ea6d9
bartheijenk <bdheijenk@gmail.com>
2021-06-14 13:10:34 +0200

    refactor

--- src/app/modules/search/components/advanced-search/recept-filters/recept-filters.component.ts
+++ src/app/modules/search/components/advanced-search/recept-filters/recept-filters.component.ts
@@ -29,34 +29,74 @@ export class ReceptFiltersComponent implements OnInit {
     private categorieService: CategorieService,
     private ingredientService: IngredientService
   ) {
+    this.initFilterForm();
+    this.initIngredientenFilters();
+    this.initCategorieFilters();
+  }
 
+  ngOnInit() {
+    this.initReceptenSubject();
+  }
+
+  private initReceptenSubject() {
+    this.searchService.receptenSub$.subscribe(recepten$ => {
+      recepten$.subscribe(recepten => {
+        let { currentCategories, currentIngredients }: { currentCategories: string[]; currentIngredients: string[]; } = this.fillCurrentArrays(recepten);
+        ({ currentCategories, currentIngredients } = this.filterArrays(currentCategories, currentIngredients));
+        this.subscribeForControlDisable(currentCategories, currentIngredients);
+      });
+    });
+  }
+
+  private fillCurrentArrays(recepten: Recept[]) {
+    let currentCategories: Array<string> = [];
+    let currentIngredients: Array<string> = [];
+    recepten.forEach(r => {
+      r.ingredienten.forEach(i => {
+        currentIngredients.push(i.ingredient.id);
+      });
+      r.categories.forEach(c => {
+        currentCategories.push(c.id);
+      });
+    });
+    return { currentCategories, currentIngredients };
+  }
+
+  private filterArrays(currentCategories: string[], currentIngredients: string[]) {
+    currentCategories = this._catMap.filter(c => !currentCategories.includes(c));
+    currentIngredients = this._ingrMap.filter(i => !currentIngredients.includes(i));
+    return { currentCategories, currentIngredients };
+  }
+
+  private subscribeForControlDisable(currentCategories: string[], currentIngredients: string[]) {
+    (this.categories.controls as Array<FormGroup>).forEach(
+      f => {
+        if (currentCategories.includes(f.value.id)) {
+          f.disable({ emitEvent: false });
+        } else {
+          f.enable({ emitEvent: false });
+        }
+      });
+    (this.ingredienten.controls as Array<FormGroup>).forEach(f => {
+      if (currentIngredients.includes(f.value.id)) {
+        f.disable({ emitEvent: false });
+      } else {
+        f.enable({ emitEvent: false });
+      }
+    });
+  }
+
+  private initFilterForm() {
     this.filter = this.formBuilder.group({
       maxSer: [this.maxServings],
       minSer: [this.minServings],
       categories: this.formBuilder.array([]),
       ingredienten: this.formBuilder.array([]),
       bronnen: this.formBuilder.array([])
-
     });
+  }
 
-    this.ingredientService.getAllIngredients().subscribe(ingrs => {
-      ingrs.forEach(ingr => {
-        if(!this._ingrMap.includes(ingr.id))
-          this._ingrMap.push(ingr.id)
-        const group = this.formBuilder.group({
-          id: ingr.id,
-          naam: ingr.naam,
-          waarde: false
-        });
-        this.ingredienten.push(group);
-        group.valueChanges.pipe(
-          debounceTime(20)
-        ).subscribe(() => {
-          this.updateFilters()
-        })
-      })
-    })
-
+  private initCategorieFilters() {
     this.categorieService.getAllCategories().subscribe(cats => {
       cats.forEach(cat => {
         if (!this._catMap.includes(cat.id))
@@ -70,47 +110,28 @@ export class ReceptFiltersComponent implements OnInit {
         group.valueChanges.pipe(
           debounceTime(20)
         ).subscribe(() => {
-          this.updateFilters()
-        })
-      })
-    })
-
-  }
-  ngOnInit() {
-
-    this.initReceptenSubject();
-  }
-
-  private initReceptenSubject() {
-    this.searchService.receptenSub$.subscribe(recepten$ => {
-      recepten$.subscribe(recepten => {
-        let currentCategories: Array<string> = [];
-        let currentIngredients: Array<string> = [];
-        recepten.forEach(r => {
-          r.ingredienten.forEach(i => {
-            currentIngredients.push(i.ingredient.id);
-          })
-          r.categories.forEach(c => {
-            currentCategories.push(c.id);
-          });
+          this.updateFilters();
         });
-        currentCategories = this._catMap.filter(c => !currentCategories.includes(c));
-        currentIngredients = this._ingrMap.filter(i => !currentIngredients.includes(i));
-        (this.categories.controls as Array<FormGroup>).forEach(
-          f => {
-            if (currentCategories.includes(f.value.id)) {
-              f.disable({ emitEvent: false });
-            } else {
-              f.enable({ emitEvent: false });
-            }
-          });
-          (this.ingredienten.controls as Array<FormGroup>).forEach( f =>{
-            if(currentIngredients.includes(f.value.id)) {
-              f.disable({emitEvent: false})
-            } else {
-              f.enable({ emitEvent: false });
-            }
-          });
+      });
+    });
+  }
+
+  private initIngredientenFilters() {
+    this.ingredientService.getAllIngredients().subscribe(ingrs => {
+      ingrs.forEach(ingr => {
+        if (!this._ingrMap.includes(ingr.id))
+          this._ingrMap.push(ingr.id);
+        const group = this.formBuilder.group({
+          id: ingr.id,
+          naam: ingr.naam,
+          waarde: false
+        });
+        this.ingredienten.push(group);
+        group.valueChanges.pipe(
+          debounceTime(20)
+        ).subscribe(() => {
+          this.updateFilters();
+        });
       });
     });
   }
@@ -143,7 +164,6 @@ export class ReceptFiltersComponent implements OnInit {
     return this.ingredienten.at(i) as FormGroup
   }
 
-
   getCategoryFormGroup(i: number) {
     return this.categories.at(i) as FormGroup
   }
@@ -151,4 +171,6 @@ export class ReceptFiltersComponent implements OnInit {
   getCategoryFormControl(i: number) {
     return this.getCategoryFormGroup(i).get("waarde") as FormControl;
   }
+
+
 }
